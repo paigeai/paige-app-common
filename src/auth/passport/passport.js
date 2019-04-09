28c99d91c12ac4ecc43733498ace74474cb030f2
Arya Mohades <aryamohades@gmail.com>
2019-04-09 02:25:31 -0500

    refactor, remove unused package

--- src/auth/passport/passport.js
+++ src/auth/passport/passport.js
@@ -5,43 +5,43 @@ const { compare } = require('../crypto');
 
 const { APP_SECRET } = process.env;
 
-// Define jwt authentication options
-const jwtOpts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
-  secretOrKey: APP_SECRET,
-};
-
-// Define local login options
-const localOpts = {
-  usernameField: 'email',
-  passwordField: 'password',
-};
-
-// Initialize local login strategy
-const localStrategy = new LocalStrategy(localOpts, async (email, password, done) => {
-  const user = await User.query()
-    .select('*')
-    .findOne({ email });
-
-  if (!user) {
-    return done(null, false);
-  }
-
-  const match = await compare(password, user.password);
-
-  return match ? done(null, user) : done(null, false);
-});
-
-// Initialize jwt authentication strategy
-const jwtStrategy = new Strategy(jwtOpts, async (payload, done) => {
-  const user = await User.query()
-    .select('*')
-    .findById(payload.id);
-
-  return user ? done(null, user) : done(null, false);
-});
-
 module.exports = passport => {
+  // Define jwt authentication options
+  const jwtOpts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
+    secretOrKey: APP_SECRET,
+  };
+
+  // Define local login options
+  const localOpts = {
+    usernameField: 'email',
+    passwordField: 'password',
+  };
+
+  // Initialize local login strategy
+  const localStrategy = new LocalStrategy(localOpts, async (email, password, done) => {
+    const user = await User.query()
+      .select('*')
+      .findOne({ email });
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    const match = await compare(password, user.password);
+
+    return match ? done(null, user) : done(null, false);
+  });
+
+  // Initialize jwt authentication strategy
+  const jwtStrategy = new Strategy(jwtOpts, async (payload, done) => {
+    const user = await User.query()
+      .select('*')
+      .findById(payload.id);
+
+    return user ? done(null, user) : done(null, false);
+  });
+
   passport.use(localStrategy);
   passport.use(jwtStrategy);
 };
