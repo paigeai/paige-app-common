5729a2c7ff64f6536daeba316ba1bd28be82ef2f
Arya Mohades <aryamohades@gmail.com>
2019-04-09 02:35:22 -0500

    fix app routes

--- src/app/app.js
+++ src/app/app.js
@@ -67,43 +67,19 @@ module.exports = options => {
   );
 
   //
+  // Setup healthcheck route.
+  //
+  app.get('/healthz', (req, res) => {
+    res.sendStatus(200);
+  });
+
+  //
   // Optionally initialize passport and protected routes.
   //
   if (options.auth) {
     app.use(passport.initialize());
     auth.passport(passport);
     app.passport = passport;
-
-    //
-    // Setup heartbeat route.
-    //
-    app.get('/healthz', (req, res) => {
-      res.sendStatus(200);
-    });
-
-    //
-    // Setup server status route.
-    //
-    app.get('/status', (req, res) => {
-      if (req.query.info) {
-        res.send({
-          status: 'up',
-          node: {
-            version: process.version,
-            memoryUsage: Math.round(process.memoryUsage().rss / 1024 / 1024) + 'M',
-            uptime: process.uptime(),
-          },
-          system: {
-            loadavg: os.loadavg(),
-            freeMemory: Math.round(os.freemem() / 1024 / 1024) + 'M',
-          },
-          env: process.env.NODE_ENV,
-          hostname: os.hostname(),
-        });
-      } else {
-        res.send({ status: 'up' });
-      }
-    });
   }
 
   //
